e5f1ff024b16e5fe4ae382584fdebf641609ea8d
Alex <sshybee6@gmail.com>
2020-02-01 10:12:23 -0600

    adjusted CAN constants to be correct

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,8 +17,8 @@ package frc.robot;
  */
 public final class Constants {
     public static final int FRONT_LEFT_MOTOR = 0;
-    public static final int FRONT_RIGHT_MOTOR = 1;
-    public static final int BACK_LEFT_MOTOR = 2;
+    public static final int BACK_LEFT_MOTOR = 1;
+    public static final int FRONT_RIGHT_MOTOR = 2;
     public static final int BACK_RIGHT_MOTOR = 3;
 
     public static final int RIGHT_ENCODER_1 = 1;
