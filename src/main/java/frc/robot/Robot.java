45b28de39d42a6efb0d8d3d5c5554adee6dc70e3
AScheibe <35386636+AScheibe@users.noreply.github.com>
2020-01-31 21:47:32 -0600

    added ability to see gyro readings on smart dashboard of all 3 axis

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,12 +8,17 @@
 //Test
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DriveContinous;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.utilities.PIDControlLoop;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -25,7 +30,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
   public static DriveTrain driveTrain;
-
+  public static AHRS ahrs;
   
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -38,6 +43,7 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
     driveTrain = new DriveTrain();
     PIDControlLoop p = new PIDControlLoop();
+    ahrs = new AHRS(SPI.Port.kMXP);
   }
 
   /**
@@ -96,7 +102,7 @@ public class Robot extends TimedRobot {
     // this line or comment it out.
     CommandScheduler.getInstance().schedule(new DriveContinous());
     CommandScheduler.getInstance().run();
-    
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -107,6 +113,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    SmartDashboard.putNumber("Gyro X", ahrs.getRawGyroX());
+    SmartDashboard.putNumber("Gyro Y", ahrs.getRawGyroY());
+    SmartDashboard.putNumber("Gyro Z", ahrs.getRawGyroZ());
   }
 
   @Override
